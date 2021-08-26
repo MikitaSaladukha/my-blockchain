ff4926ed2417efd4f7864e01eb566ac0b926f1ee
Mikita Saladukha <mikita.saladukha@gmail.com>
2021-08-26 12:25:39 +0300

    return to first block

--- src/node/main.py
+++ src/node/main.py
@@ -4,10 +4,11 @@ from common.initialize_blockchain import initialize_blockchain
 from common.io_blockchain import get_blockchain_from_memory
 from node.new_block_validation.new_block_validation import NewBlock, NewBlockException
 from node.transaction_validation.transaction_validation import Transaction, TransactionException
+from common.block import Block
 
 app = Flask(__name__)
 initialize_blockchain()
-
+##x=0
 
 @app.route("/block", methods=['POST'])
 def validate_block():
@@ -41,6 +42,7 @@ def validate_transaction():
 
 @app.route("/block", methods=['GET'])
 def get_blocks():
+    print("Mikkita")
     blockchain_base = get_blockchain_from_memory()
     return jsonify(blockchain_base.to_dict)
 
@@ -55,3 +57,28 @@ def get_user_utxos(user):
 def get_transaction(transaction_hash):
     blockchain_base = get_blockchain_from_memory()
     return jsonify(blockchain_base.get_transaction(transaction_hash))
+
+###########################################################################
+
+
+@app.route("/first_block", methods=['GET'])
+def get_first_block():
+    print("MikkiTA start")
+    blockchain_base = get_blockchain_from_memory()
+    first=first_from_last(blockchain_base)
+    print("MikkiTA end")
+    return jsonify(first.to_dict)
+
+def first_from_last(block_object)-> Block:
+    if block_object is None:
+        print("error - None block")
+        return None
+    elif block_object.previous_block is None:
+        print("first good")
+        return block_object
+    else:
+        ##x=x+1
+        ##print(x)
+        print("iteration")
+        return first_from_last(block_object.previous_block)
+    
